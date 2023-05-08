a4330c85c26533f808da5517dcd7d8951d45013b
Kubanych22 <z200707@mail.ru>
2023-05-08 17:32:14 +0600

    background video for location

--- src/index.js
+++ src/index.js
@@ -11,20 +11,6 @@ import {sliderInit} from './modules/sliders';
 sliderInit('.about__slider', {
   pagination: {
     el: '.about__slider-pagination'
-    // breakpoints: {
-    //   768: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 20
-    //   },
-    //   1024: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 26
-    //   },
-    //   1240: {
-    //     slidesPerView: 4,
-    //     spaceBetween: 30
-    //   }
-    // }
   }
 });
 
@@ -48,6 +34,13 @@ sliderInit('.career__slider', {
 const videoBG = document.querySelector('.video-bg');
 
 videoBG.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
+
+const videoBGLoc = document.querySelector('.location__video-bg');
+
+videoBGLoc.innerHTML = `
   <source src="video/video.webm" type="video/webm">
   <source src="video/video.mp4" type="video/mp4">
 `;
